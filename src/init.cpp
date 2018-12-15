694aacd1127751742975cd9e5b715fc5fa772290
Alex Wu <dindinw@users.noreply.github.com>
2018-12-15 21:17:26 +0800

    rollback changes to keep identical with upstream code

--- src/init.cpp
+++ src/init.cpp
@@ -59,9 +59,6 @@
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/replace.hpp>
 #include <boost/algorithm/string/split.hpp>
-#include <boost/bind.hpp>
-#include <boost/thread.hpp>
-#include <openssl/crypto.h>
 
 #if ENABLE_ZMQ
 #include <zmq/zmqabstractnotifier.h>
