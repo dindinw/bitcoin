c0b3328b976b1ed4c877b152251422f5310ccaf0
Alex Wu <dindinw@users.noreply.github.com>
2018-12-15 20:08:33 +0800

    cmake : refactoring

--- build_cmake/CMakeModules/FindQt5.cmake
+++ build_cmake/CMakeModules/FindQt5.cmake
@@ -11,4 +11,23 @@ MESSAGE(STATUS "find QT ${PKG_QT5_PREFIX}")
 MESSAGE(STATUS "find QT ${PKG_QT5_INCLUDEDIR}")
 MESSAGE(STATUS "find QT ${PKG_QT5_LIBDIR}")
 
+# TODO refactor
+if(ENABLE_QT)
+    # Qt5
+    MESSAGE(STATUS "try Found qt5 libraries")
+    list(APPEND CMAKE_PREFIX_PATH "/usr/local/opt/qt/")
+
+    FIND_PACKAGE(Qt5Core CONFIG REQUIRED)
+    IF (Qt5Core_FOUND)
+        MESSAGE(STATUS "add qt5 include dir ${Qt5Core_INCLUDE_DIRS}")
+        include_directories(${Qt5Core_INCLUDE_DIRS})
+    ENDIF(Qt5Core_FOUND)
+
+    FIND_PACKAGE(Qt5Test CONFIG REQUIRED)
+    IF (Qt5Test_FOUND)
+        MESSAGE(STATUS "add qt5 include dir ${Qt5Test_INCLUDE_DIRS}")
+        include_directories(${Qt5Test_INCLUDE_DIRS})
+    ENDIF(Qt5Test_FOUND)
+endif()
+
 
