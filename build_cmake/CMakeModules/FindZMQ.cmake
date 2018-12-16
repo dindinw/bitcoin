b6ae09a2a8affb1867476b04aebe32e3e637cf54
Alex Wu <dindinw@users.noreply.github.com>
2018-12-16 21:39:43 +0800

    cmake : refactor find zmq && polish status msg

--- build_cmake/CMakeModules/FindZMQ.cmake
+++ build_cmake/CMakeModules/FindZMQ.cmake
@@ -7,3 +7,5 @@ set ( ZMQ_INCLUDE_DIRS ${ZMQ_INCLUDE_DIR} )
 include ( FindPackageHandleStandardArgs )
 
 find_package_handle_standard_args ( ZMQ DEFAULT_MSG ZMQ_LIBRARY ZMQ_INCLUDE_DIR )
+
+message(STATUS "Found libzmq at ${ZMQ_INCLUDE_DIRS} ${ZMQ_LIBRARY}")
