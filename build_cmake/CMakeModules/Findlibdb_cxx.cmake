bb8304d9e471ce9f129375df0abd255d3ca85aeb
Alex Wu <dindinw@users.noreply.github.com>
2018-12-16 12:14:36 +0800

    cmake : refactor bdb only required when wallet enabled

--- build_cmake/CMakeModules/Findlibdb_cxx.cmake
+++ build_cmake/CMakeModules/Findlibdb_cxx.cmake
@@ -1,5 +1,9 @@
 MESSAGE(STATUS "Using Findlibdb_cxx.cmake...")
-
+if (NOT "$ENV{LIBDB_CXX_ROOT}" STREQUAL "")
+    set(ENV{LIBDB_CXX_DIR} $ENV{LIBDB_CXX_ROOT})
+    set(LIBDB_CXX_INCLUDE_DIR $ENV{LIBDB_CXX_ROOT}/include)
+    set(LIBDB_CXX_LIBRARIES $ENV{LIBDB_CXX_ROOT}/lib )
+endif()
 find_path(LIBDB_CXX_INCLUDE_DIR db_cxx.h
         /usr/include/
         /usr/local/include/
@@ -10,3 +14,4 @@ find_library(LIBDB_CXX_LIBRARIES NAMES db_cxx
 
 mark_as_advanced(LIBDB_CXX_INCLUDE_DIR LIBDB_CXX_LIBRARIES )
 
+MESSAGE(STATUS "Found libdb at ${LIBDB_CXX_INCLUDE_DIR} and ${LIBDB_CXX_LIBRARIES}")
