f2c996470c2bbbf8644ae9b7b55be452395e11d2
Alex Wu <dindinw@users.noreply.github.com>
2018-12-20 09:49:26 +0800

    cmake : add miniupnpc

--- build_cmake/CMakeModules/FindLibminiupnpc.cmake
+++ build_cmake/CMakeModules/FindLibminiupnpc.cmake
@@ -1,3 +1,26 @@
 if (NOT "$ENV{LIBMINUPNPC_ROOT}" STREQUAL "")
-    set(LLIBMINUPNPC_DIR $ENV{LIBMINUPNPC_ROOT})
+    set(LIBMINUPNPC_DIR $ENV{LIBMINUPNPC_ROOT})
 endif()
+
+set(Libminiupnpc_EXTRA_PREFIXES /usr/local /opt/local "$ENV{HOME}" ${LLIBMINUPNPC_DIR})
+foreach(prefix ${Libminiupnpc_EXTRA_PREFIXES})
+    list(APPEND Libminiupnpc_INCLUDE_PATHS "${prefix}/include" "${prefix}/include/miniupnpc")
+    list(APPEND Libminiupnpc_LIBRARIES_PATHS "${prefix}/lib")
+endforeach()
+
+find_path (LIBMINIUPNPC_INCLUDE_DIR miniupnpc.h
+        NAMES miniupnpc.h
+        PATHS ${Libminiupnpc_INCLUDE_PATHS})
+find_library (LIBMINIUPNPC_LIB
+        NAMES miniupnpc libminiupnpc
+        PATHS ${Libminiupnpc_LIBRARIES_PATHS})
+
+
+list (APPEND LIBMINUPNPC_INCLUDE_DIRS ${LIBMINIUPNPC_INCLUDE_DIR})
+list (APPEND LIBMINIUPNPC_LIBRARIES   ${LIBMINIUPNPC_LIB})
+
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args (Libminiupnpc DEFAULT_MSG LIBMINUPNPC_INCLUDE_DIRS LIBMINIUPNPC_LIBRARIES)
+mark_as_advanced(LIBMINUPNPC_INCLUDE_DIRS LIBMINIUPNPC_LIBRARIES)
+
+message(STATUS "Found Libminiupnpc : ${LIBMINUPNPC_INCLUDE_DIRS} ${LIBMINIUPNPC_LIBRARIES}")
