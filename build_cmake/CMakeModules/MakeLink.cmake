b7d09ad86ddaedbac81fb648f1a83e1fa2d75872
Alex Wu <dindinw@users.noreply.github.com>
2018-12-20 11:17:04 +0800

    cmake : better config.h symlink

--- build_cmake/CMakeModules/MakeLink.cmake
+++ build_cmake/CMakeModules/MakeLink.cmake
@@ -1,8 +1,5 @@
-macro(make_link src dest target)
-   add_custom_target(${target}
-           COMMAND ${CMAKE_COMMAND} -E create_symlink ${src} ${dest}
-           DEPENDS  ${dest}
-           COMMENT "mklink ${src} -> ${dest}")
-   # add_custom_command(TARGET ${target} PRE_BUILD
-   #         COMMAND ${CMAKE_COMMAND} -E create_symlink ${src} ${dest} DEPENDS  ${dest} COMMENT "mklink ${src} -> ${dest}")
+macro(make_link src dest)
+message(STATUS "mklink ${src} -> ${dest}")
+execute_process(
+            COMMAND ${CMAKE_COMMAND} -E create_symlink ${src} ${dest})
 endmacro()
