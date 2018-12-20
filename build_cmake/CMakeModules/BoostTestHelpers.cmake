17824932ae718046ee6907cef5f1e1f513f9d359
Alex Wu <dindinw@users.noreply.github.com>
2018-12-20 08:46:24 +0800

    cmake : fix boost test finding

--- build_cmake/CMakeModules/BoostTestHelpers.cmake
+++ build_cmake/CMakeModules/BoostTestHelpers.cmake
@@ -1,15 +1,24 @@
 function(add_boost_test TEST_EXECUTABLE_NAME)
     foreach(f ${ARGN})
         # message(STATUS "f=${f}")
-        get_filename_component(TEST_SUITE_NAME ${f} NAME_WE)
+        get_filename_component(TEST_FILE_NAME ${f} NAME_WE)
         file(READ "${f}" SOURCE_FILE_CONTENTS)
         set(FOUND_TEST_SUITE "")
-        string(REGEX MATCH "BOOST_FIXTURE_TEST_SUITE"
+        string(REGEX MATCH "BOOST_FIXTURE_TEST_SUITE\\( *([A-Za-z_0-9]+), *([A-Za-z_0-9]+) *\\)"
                 FOUND_TEST_SUITE ${SOURCE_FILE_CONTENTS})
-        string(REGEX MATCH "BOOST_AUTO_TEST_SUITE"
+        if(FOUND_TEST_SUITE)
+            string(REGEX REPLACE ".*\\( *([A-Za-z_0-9]+).* *\\).*" "\\1" TEST_SUITE_NAME ${FOUND_TEST_SUITE})
+        endif()
+        string(REGEX MATCH "BOOST_AUTO_TEST_SUITE\\( *([A-Za-z_0-9]+) *\\)"
                 FOUND_TEST_SUITE ${SOURCE_FILE_CONTENTS})
-        if(NOT ${FOUND_TEST_SUITE} STREQUAL "")
-            # message(STATUS "find on ${TEST_SUITE_NAME}")
+        if(FOUND_TEST_SUITE)
+            string(REGEX REPLACE ".*\\( *([A-Za-z_0-9]+) *\\).*" "\\1" TEST_SUITE_NAME ${FOUND_TEST_SUITE})
+        endif()
+        if(NOT ${TEST_SUITE_NAME} STREQUAL "")
+            if (NOT "${TEST_FILE_NAME}" STREQUAL "${TEST_SUITE_NAME}")
+                # message(STATUS "WARNNING: Test file name : ${TEST_FILE_NAME} != Test suite name : ${TEST_SUITE_NAME}")
+            endif()
+            # message(STATUS "try to find match on test suite : ${TEST_SUITE_NAME}")
             string(REGEX MATCHALL "BOOST_AUTO_TEST_CASE\\( *([A-Za-z_0-9]+) *\\)"
                     FOUND_TESTS ${SOURCE_FILE_CONTENTS})
             foreach(HIT ${FOUND_TESTS})
@@ -19,6 +28,18 @@ function(add_boost_test TEST_EXECUTABLE_NAME)
                         COMMAND ${TEST_EXECUTABLE_NAME}
                         --run_test=${TEST_SUITE_NAME}/${TEST_NAME} --catch_system_error=yes)
             endforeach()
+            # also need to match BOOST_FIXTURE_TEST_CASE
+            set(FOUND_TESTS "")
+            string(REGEX MATCHALL "BOOST_FIXTURE_TEST_CASE\\( *([A-Za-z_0-9]+), *([A-Za-z_0-9]+) *\\)"
+                    FOUND_TESTS ${SOURCE_FILE_CONTENTS})
+            foreach(HIT ${FOUND_TESTS})
+                string(REGEX REPLACE ".*\\( *([A-Za-z_0-9]+).* *\\).*" "\\1" TEST_NAME ${HIT})
+                # message(STATUS "add test ${TEST_SUITE_NAME}.${TEST_NAME}")
+                add_test(NAME "${TEST_SUITE_NAME}.${TEST_NAME}"
+                        COMMAND ${TEST_EXECUTABLE_NAME}
+                        --run_test=${TEST_SUITE_NAME}/${TEST_NAME} --catch_system_error=yes)
+            endforeach()
+            set(TEST_SUITE_NAME "")
         endif()
     endforeach(f)
 endfunction()
