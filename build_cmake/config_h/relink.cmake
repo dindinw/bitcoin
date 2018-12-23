4a3bfe533903cd15fd7b00298f808e21b7c77a17
Alex Wu <dindinw@users.noreply.github.com>
2018-12-23 17:33:26 +0800

    cmake : let config-h to work with multiple-config

---
+++ build_cmake/config_h/relink.cmake
@@ -0,0 +1,5 @@
+include(${CMAKE_CURRENT_BINARY_DIR}/../../CMakeModules/MakeLink.cmake)
+message(STATUS "relink config : ${BITCOIN_CONFIG_CMAKE} => ${BITCOIN_CONFIG})")
+
+
+#make_link(${BITCOIN_CONFIG_CMAKE} ${BITCOIN_CONFIG})
