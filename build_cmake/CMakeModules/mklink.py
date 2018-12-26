bb1202247d72336d7460cccc1ef0438037e87c74
Alex Wu <dindinw@users.noreply.github.com>
2018-12-26 16:37:03 +0800

    cmake : remove the old link if exist : mklink.py

--- build_cmake/CMakeModules/mklink.py
+++ build_cmake/CMakeModules/mklink.py
@@ -6,12 +6,16 @@ import platform
 
 
 def main(src, dst):
+    if os.path.isfile(dst) and os.path.islink(dst):
+        os.remove(dst)
+        # print('old symlink "%s" removed' % dst)
     symlink(src, dst)
     print("symlink created")
 
 
 # Note : the original _symlink_win32 code copied from
 # https://github.com/erdc/python/blob/v2.7.4/Lib/test/symlink_support.py
+# The _symlink_win32 method using the win32 API only provided by ctypes
 def _symlink_win32(target, link, target_is_directory=False):
     """
     Ctypes symlink implementation since Python doesn't support
@@ -46,7 +50,7 @@ def _symlink_win32(target, link, target_is_directory=False):
 
     target_is_directory = target_is_directory or os.path.isdir(target)
 
-    if os.path.isfile(link) and islink(link) :
+    if islink(link) :
         # check if the link already exist and del it
         # need to make sure we can only delete the file if it's a link, not a real file
         if not DeleteFile(link):
