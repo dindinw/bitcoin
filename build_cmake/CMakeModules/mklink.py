4b96afbc5c2672b15372d7a5838934ff6eca8bb2
Alex Wu <dindinw@users.noreply.github.com>
2018-12-26 11:40:12 +0800

    refactor the python script for make link

--- build_cmake/CMakeModules/mklink.py
+++ build_cmake/CMakeModules/mklink.py
@@ -26,52 +26,29 @@ def _symlink_win32(target, link, target_is_directory=False):
     )
     CreateSymbolicLink.restype = ctypes.wintypes.BOOLEAN
 
-    def format_system_message(errno):
-        """
-        Call FormatMessage with a system error number to retrieve
-        the descriptive error message.
-        """
-        # first some flags used by FormatMessageW
-        ALLOCATE_BUFFER = 0x100
-        ARGUMENT_ARRAY = 0x2000
-        FROM_HMODULE = 0x800
-        FROM_STRING = 0x400
-        FROM_SYSTEM = 0x1000
-        IGNORE_INSERTS = 0x200
+    DeleteFile = ctypes.windll.kernel32.DeleteFileW
+    DeleteFile.argtypes = (ctypes.c_wchar_p,)
+    DeleteFile.restype = ctypes.c_bool
 
-        # Let FormatMessageW allocate the buffer (we'll free it below)
-        # Also, let it know we want a system error message.
-        flags = ALLOCATE_BUFFER | FROM_SYSTEM
-        source = None
-        message_id = errno
-        language_id = 0
-        result_buffer = ctypes.wintypes.LPWSTR()
-        buffer_size = 0
-        arguments = None
-        bytes = ctypes.windll.kernel32.FormatMessageW(
-            flags,
-            source,
-            message_id,
-            language_id,
-            ctypes.byref(result_buffer),
-            buffer_size,
-            arguments,
-        )
-        # note the following will cause an infinite loop if GetLastError
-        #  repeatedly returns an error that cannot be formatted, although
-        #  this should not happen.
-        handle_nonzero_success(bytes)
-        message = result_buffer.value
-        ctypes.windll.kernel32.LocalFree(result_buffer)
-        return message
+    GetFileAttributesW = ctypes.windll.kernel32.GetFileAttributesW
+    GetFileAttributesW.argtypes = (ctypes.c_wchar_p,)
+    GetFileAttributesW.restype = ctypes.c_uint
+
+    def islink(path):
+        FILE_ATTRIBUTE_REPARSE_POINT = 1024
+        return bool(GetFileAttributesW(path) & FILE_ATTRIBUTE_REPARSE_POINT)
 
     def handle_nonzero_success(result):
         if result == 0:
             value = ctypes.windll.kernel32.GetLastError()
-            strerror = format_system_message(value)
+            strerror = (value)
             raise WindowsError(value, strerror)
 
     target_is_directory = target_is_directory or os.path.isdir(target)
+    if os.path.isfile(target) and islink(target) :
+        # only delete link not the real file if the target is
+        if not DeleteFile(target):
+            raise WindowsError('could not delete file "%s"' % target)
     handle_nonzero_success(CreateSymbolicLink(link, target, target_is_directory))
 
 
