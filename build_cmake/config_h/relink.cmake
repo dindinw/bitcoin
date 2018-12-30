933dd7bcaed7a27d4ddc8abae01dc0f580157a0f
Alex Wu <dindinw@users.noreply.github.com>
2018-12-30 16:20:38 +0800

    cmake : fix module path not work on windows

--- build_cmake/config_h/relink.cmake
+++ build_cmake/config_h/relink.cmake
@@ -1,5 +1,6 @@
 # using the MakeLinke.cmake to do the relink of config files
 # the arguments can be passed from outside by using cmake -D var=value
-include(${CMAKE_CURRENT_BINARY_DIR}/../../CMakeModules/MakeLink.cmake)
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../../CMakeModules")
+include(MakeLink)
 make_link(${BITCOIN_CONFIG_CMAKE} ${BITCOIN_CONFIG})
 message(STATUS "relink ${BITCOIN_CONFIG_CMAKE} => ${BITCOIN_CONFIG} ok")
